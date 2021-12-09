d2ae12059077c95d552385b80cab0adb8d366ff5
Iuribabalin <babalin2011@gmail.com>
2021-12-09 23:03:00 +0300

    каритинка

---
+++ src/components/LeftPanelAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card width="40%" height="100%" color="orange darken-1">
+    <div style="margin-top: 15%">
+<!--      <v-img/>-->
+    <v-text class="font-weight-medium" style="font-size: 30pt; margin-left: 20%;">
+      City Project
+    </v-text>
+    <v-img src="../assets/city.jpg"
+           class="ml-auto mr-auto"
+           width="60%"
+           height="70%"
+           style="border-radius:40px; margin-top: 20px  "
+    />
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "LeftPanelAuth",
+}
+</script>
+
---
+++ src/views/Auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <LeftPanelAuth/>
+  </v-app>
+</template>
+
+<script>
+import LeftPanelAuth from "../components/LeftPanelAuth";
+
+export default {
+  name: "Auth",
+  components: {LeftPanelAuth}
+}
+</script>
